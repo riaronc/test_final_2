57fcf2f3b38c7c91140e5a48389a11d3cc922610
riaronc <44467557+riaronc@users.noreply.github.com>
2024-01-09 17:42:39 +0200

    commit message

---
+++ ft_strmapi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odushko <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/31 12:24:49 by odushko           #+#    #+#             */
+/*   Updated: 2018/10/31 12:32:18 by odushko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
+
+	if (s && f)
+	{
+		i = 0;
+		len = ft_strlen(s);
+		str = (char *)malloc((len + 1) * sizeof(*str));
+		if (!str)
+			return (NULL);
+		while (i < len)
+		{
+			str[i] = f(i, s[i]);
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
