cfdc1844ad13972693cd20ac2185a221bfa5ed9d
riaronc <44467557+riaronc@users.noreply.github.com>
2024-01-09 17:42:37 +0200

    commit message

---
+++ ft_strmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odushko <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/31 11:31:04 by odushko           #+#    #+#             */
+/*   Updated: 2018/10/31 11:55:45 by odushko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
+
+	if (s && f)
+	{
+		i = 0;
+		len = ft_strlen((char *)s);
+		str = (char *)malloc(sizeof(*str) * len + 1);
+		if (!str)
+			return (NULL);
+		while (s[i] != '\0')
+		{
+			str[i] = f(s[i]);
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
